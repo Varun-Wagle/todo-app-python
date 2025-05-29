0c4a7b41e6304b6f2dd999b3f89079a2cb17aaa2
Varun Wagle <vrnwagle93@gmail.com>
2025-05-29 17:49:29 +0530

    Updated files: Added for streamlit compatibility

--- todo-streamlit.py
+++ todo-streamlit.py
@@ -21,42 +21,42 @@ if username:
                     task, priority = line.strip().split('|')
                     tasks[task] = priority
         return tasks
-
-tasks = load_tasks(task_file)
-done_tasks = load_tasks(done_file)
-
-# Add new task
-new_task = st.text_input("Add a new task: ")
-priority = st.selectbox("Select priority:", ['Low', 'Medium', 'High'])
-if st.button("Add Task"):
-    if new_task:
-        tasks[new_task] = priority
-        with open(task_file, 'a') as f:
-            f.write(f"{new_task}|{priority}\n")
-        st.success("Task Added.")
-
-#View Tasks
-st.subheader("View Tasks:")
-for task, priority in tasks.items():
-    if st.button(f"Mark 'task' as done"):
-        done_tasks[task] = priority
-        tasks.pop(task)
-        with open(done_file, 'a') as f:
-            f.write(f"{task}|{priority}|{datetime.now()}\n")
-        with open(task_file, 'w') as f:
-            for t, p in tasks.items():
-                f.write(f"Marked '{task}' as done!")
-
-if tasks:
-    for task, priority in tasks.items():
-        st.write(f"Task: {task}, Priority: {priority}")
-else:
-    st.info("No active tasks.")
-
-# View Completed Tasks
-st.subheader("Completed Tasks.")
-if done_tasks:
-    for task, priority in done_tasks.items():
-        st.write(f"Task: {task}, Priority: {priority}")
-else:
-    st.info("No Completed Tasks.")
+    tasks = load_tasks(task_file)
+    done_tasks = load_tasks(done_file)
+    
+    # Add new task
+    new_task = st.text_input("Add a new task: ")
+    priority = st.selectbox("Select priority:", ['Low', 'Medium', 'High'])
+    if st.button("Add Task"):
+        if new_task:
+            tasks[new_task] = priority
+            with open(task_file, 'a') as f:
+                f.write(f"{new_task}|{priority}\n")
+            st.success("Task Added.")
+            
+    #View Tasks
+    st.subheader("View Tasks:")
+    if tasks:
+        for task, priority in tasks.items():
+            if st.button(f"Mark 'task' as done"):
+                done_tasks[task] = priority
+                tasks.pop(task)
+                with open(done_file, 'a') as f:
+                     f.write(f"{task}|{priority}|{datetime.now()}\n")
+                with open(task_file, 'w') as f:
+                     for t, p in tasks.items():
+                          f.write(f"Marked '{task}' as done!")
+            
+            for task, priority in tasks.items():
+                 st.write(f"Task: {task}, Priority: {priority}")
+    
+    else:
+        st.info("No active tasks.")
+        
+    # View Completed Tasks
+    st.subheader("Completed Tasks.")
+    if done_tasks:
+        for task, priority in done_tasks.items():
+            st.write(f"Task: {task}, Priority: {priority}")
+    else:
+        st.info("No Completed Tasks.")
